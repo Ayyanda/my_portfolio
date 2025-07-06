c33df6c4a36caf9a6320eddbf9074b6b2f84e4cc
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-07-06 22:25:45 +0200

    added theme toggle button to navigation bar and added dark and light theme to portfolio

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const typewriterText = "Hey There, I'm Ayanda.";
 const typewriterSpeed = 120;
 let i = 0;
 
+const toggleButton = document.getElementById('theme-toggle');
+const lastSavedTheme = localStorage.getItem('theme');
+
 function typeWriter() {
     if (i < typewriterText.length){
         document.getElementById("typewriter"). textContent += typewriterText.charAt(i) 
@@ -10,4 +13,23 @@ function typeWriter() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", typeWriter);
+if(lastSavedTheme == 'light'){
+    document.body.classList.add('light-mode');
+    toggleButton.textContent = '🌞';
+}
+
+toggleButton.addEventListener('click', ()=>{
+    document.body.classList.toggle('light-mode');
+
+    if(document.body.classList.contains('light-mode')){
+        localStorage.setItem('theme', 'light');
+        toggleButton.textContent = '🌞';
+    } else {
+        localStorage.setItem('theme','dark');
+        toggleButton.textContent = '🌙'
+    }
+});
+
+typeWriter();
+
+//document.addEventListener("DOMContentLoaded", typeWriter);
